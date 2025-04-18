e14a2bb8eae1b6b32db76821a4e996c2502e6e0e
RareChimera87 <sanctireinadiaz@gmail.com>
2025-04-18 00:43:44 -0500

    Fracaso de enfoque

--- core/generator.py
+++ core/generator.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 """ list = [[[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]], 
 [[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]], 
@@ -6,7 +7,7 @@ import random
  """
 list = [
 [3, 6, 2, 8, 7, 5, 4, 1, 9], 
-[4, 9, 6, 1, 3, 8, 2, 7, 5], 
+[3, 9, 6, 1, 3, 8, 2, 7, 5], 
 [6, 5, 8, 2, 9, 7, 4, 3, 1], 
 [2, 7, 9, 5, 3, 4, 1, 6, 8], 
 [2, 1, 9, 5, 4, 7, 6, 8, 3], 
@@ -24,19 +25,30 @@ def verificaTablero(lista):
     
     for i in range(len(lista)):
         currentLista = lista[i]
-        NumbersTryed = []
 
         print("------Lista en la que esta trabajando: ", currentLista, "-----------")
+        
         for j in range(len(currentLista)):
             print("Lista Actual: ", currentLista)
             NumeroValido = False
             currentNumber = currentLista[j]
+            iteraciones = 0
             
 
             print("Su Numero actual es: ", currentNumber)
+            time.sleep(5)
+            print("Lista orignal: ", tablero)
+            print("Lista por el momento: ", lista)
+            time.sleep(15)
             while NumeroValido != True:
+                print("*********Analizando lista: ", j+1, "*****")
+                iteraciones += 1
+                IsColumna = False
                 currentColumna = []
                 esUnico = True
+
+
+
 
                 for l in range(len(currentLista)):
                     currentColumna.append(lista[l][j])
@@ -48,31 +60,36 @@ def verificaTablero(lista):
                 for k in range(len(currentLista)):
                     
                     if k == j:
-                        continue
-                    elif currentNumber in NumbersTryed:
-                        print("El numero: ", currentNumber, " ya se ha validado")
+                                continue
+                    elif currentNumber == currentLista[k] or currentNumber == currentColumna[k]:
+                        if currentNumber == currentColumna[k] and currentNumber != currentLista[k]:
+                            IsColumna = True
+                            break
+                        print("El numero: ", currentNumber," ya se encuentra en la fila y/o columna")
                         esUnico = False
                         break
-                    elif currentNumber == currentLista[k] or currentNumber == 0:
-                        print("El numero: ", currentNumber," ya se encuentra en la fila")
-                        esUnico = False
-                        break
-                    """ elif currentNumber in currentColumna:
-                        print("El numero: ", currentNumber," ya se encuentra en la columna")
-                        esUnico = False
-                        break """
                         
 
 
 
                 if esUnico:
-                    print("Lista anterior: ", currentLista)
-                    print("Reemplazando: ", currentLista[j], " por: ", currentNumber)
-                    NumbersTryed.append(currentNumber)
-
-                    currentLista[j] = currentNumber
-                    print("Lista modificada: ", currentLista)
-                    NumeroValido = True
+                    if IsColumna:
+                        numero = random.randint(0, 8)
+                        while numero == j:
+                            numero = random.randint(0, 8)
+                        print("Numero: ", numero)
+                        print("El numero: ", currentNumber," ya se encuentra en la columna")
+                        print("Modificando posicion. \n Lista Anterior: ", currentLista)
+                        currentLista.insert(numero, currentLista.pop(j))
+                        print("Lista Nueva: ", currentLista)
+                        currentNumber = currentLista[j]
+                        print("Analizando ahora: ", currentNumber)
+                    else:
+                        print("Lista anterior: ", currentLista)
+                        print("Reemplazando: ", currentLista[j], " por: ", currentNumber)
+                        currentLista[j] = currentNumber
+                        print("Lista modificada: ", currentLista)
+                        NumeroValido = True
 
 
                 else:
@@ -82,7 +99,9 @@ def verificaTablero(lista):
                     if currentNumber > 9:
                         currentNumber = 1
                         print("Numero fuera de rango\n Nuevo numero: ", currentNumber)
+
             print("Su lista modificada es: ",currentLista)
+            
     print("Su nuevo tablero es: ", lista)
     return lista 
 
@@ -101,7 +120,7 @@ def verificaColumnas(lista):
 
     print("Lista que mando: ", Matriz)
     
-    listinha = verificaFilas(Matriz)
+    listinha = verificaColumnas(Matriz)
     
     print("Lista que devuelven: ", listinha)
 
@@ -123,7 +142,7 @@ def verificaColumnas(lista):
                     lista[k][i] = currentNumber
                     algo += 1
                 else:
-                    verificaFilas(listinha, currentNumber)
+                    verificaColumnas(listinha, currentNumber)
         print("Ya no se: ", lista)
         
 
