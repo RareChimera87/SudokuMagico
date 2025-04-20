9fffe7133397af74a584e3ad226e8eb9e50c5d35
RareChimera87 <sanctireinadiaz@gmail.com>
2025-04-20 11:34:24 -0500

    Limpieza

--- core/sudoku.py
+++ core/sudoku.py
@@ -1,21 +1,6 @@
 import random
 import time
 
-nose = [
-[3, 6, 2, 8, 7, 5, 4, 1, 9], 
-[4, 9, 6, 1, 3, 8, 2, 7, 5], 
-[6, 5, 8, 2, 9, 7, 4, 3, 1], 
-[2, 7, 9, 5, 3, 4, 1, 6, 8], 
-[2, 1, 9, 5, 4, 7, 6, 8, 3], 
-[1, 3, 4, 5, 9, 6, 8, 2, 7], 
-[6, 7, 1, 4, 5, 2, 8, 3, 9], 
-[6, 3, 4, 7, 8, 9, 5, 2, 1], 
-[6, 5, 4, 7, 9, 2, 3, 1, 8]
-]
-
-#Recuerda lo de probar reemplazar el len por self.size
-
-
 class Sudoku:
     def __init__(self):
         self.size = 9
